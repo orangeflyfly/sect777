204d506abb7ee2f960ba1b7f3bb16309569b1955
orangeflyfly <toyotamark02@gmail.com>
2026-04-12 18:51:13 +0800

    Update ui_shop.js

--- ui_shop.js
+++ ui_shop.js
@@ -1,1 +1,13 @@
-
+_X_UI.shop = {
+    renderShop: function(shopData) {
+        const container = document.getElementById('shop-list');
+        if (!container) return;
+        container.innerHTML = shopData.map(item => `
+            <div class="shop-card">
+                <b>${item.name}</b>
+                <p>售價：${item.price} 靈石</p>
+                <button onclick="shop.buy('${item.id}')">購買</button>
+            </div>
+        `).join('');
+    }
+};
