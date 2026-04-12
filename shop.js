1bf5e325c8671c510f97e50913c751056ba1d816
orangeflyfly <toyotamark02@gmail.com>
2026-04-12 17:40:01 +0800

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,27 +1,35 @@
 /**
- * 宗門修仙錄 - 萬寶閣模組 (shop.js) V1.4
+ * 宗門修仙錄 - 坊市模組 (shop.js) V1.4.1
  */
 function Shop(core) {
     this.core = core;
 }
 
 Shop.prototype.buy = function(shopItem) {
-    const p = this.core.player;
+    var p = this.core.player;
     if (p.data.money < shopItem.price) {
-        this.core.ui.toast("靈石不足！", "red"); return;
+        this.core.ui.toast("靈石不足，無法交易！", "red"); return;
     }
 
-    const baseTemplate = GAME_DATA.ITEMS.find(i => i.name === shopItem.name);
-    // 處理特殊商品 (如靈石袋)
     if (shopItem.name === "低階靈石袋") {
-        const bonus = 500 + Math.floor(Math.random() * 1000);
+        var bonus = 500 + Math.floor(Math.random() * 1000);
         p.data.money = p.data.money - shopItem.price + bonus;
-        this.core.ui.toast(`開啟靈石袋，獲得 🪙${bonus}`);
-    } else if (baseTemplate) {
-        const newItem = { ...baseTemplate, itemType: baseTemplate.type, count: 1 };
-        if (this.core.inventory.addItem(newItem)) {
-            p.data.money -= shopItem.price;
-            this.core.ui.toast(`購買成功`);
+        this.core.ui.toast("開啟靈石袋，獲得 🪙" + bonus);
+    } else {
+        var baseTemplate = null;
+        for (var i = 0; i < GAME_DATA.ITEMS.length; i++) {
+            if (GAME_DATA.ITEMS[i].name === shopItem.name) {
+                baseTemplate = GAME_DATA.ITEMS[i]; break;
+            }
+        }
+        if (baseTemplate) {
+            var newItem = JSON.parse(JSON.stringify(baseTemplate));
+            newItem.itemType = baseTemplate.type;
+            newItem.count = 1;
+            if (this.core.inventory.addItem(newItem)) {
+                p.data.money -= shopItem.price;
+                this.core.ui.toast("購買成功");
+            }
         }
     }
     p.save(); this.core.ui.renderAll();
