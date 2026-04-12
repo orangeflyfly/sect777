08736527ecf9fba7d2e6b8bfae1882f8b96951f2
orangeflyfly <toyotamark02@gmail.com>
2026-04-12 18:48:22 +0800

    Implement battle UI functions for HP and logging
    Add battle UI management functions for HP update and logging.

--- ui_battle.js
+++ ui_battle.js
@@ -1,1 +1,12 @@
+// ui_battle.js
+window._X_UI = window._X_UI || {}; // 如果沒有就建立一個 UI 總管物件
 
+_X_UI.battle = {
+    updateHP: function(val) { 
+        // 這裡放原本更新血條的代碼
+        console.log("血條更新中...");
+    },
+    log: function(msg) {
+        // 這裡放原本噴戰鬥日誌的代碼
+    }
+};
