4ae73ddb5167053dea41313f3bd6048fb6e9fde1
orangeflyfly <toyotamark02@gmail.com>
2026-04-14 02:21:28 +0800

    Create data_config.js

---
+++ data_config.js
@@ -0,0 +1,13 @@
+/**
+ * V1.7.0 data_config.js
+ * 職責：存放遊戲全域常數與設定
+ */
+window.GAMEDATA = window.GAMEDATA || {};
+
+GAMEDATA.CONFIG = {
+    MAX_BAG_SLOTS: 50,
+    LOG_LIMIT: 50,
+    FRAGMENT_COUNT: 5, // 殘卷合成所需數量
+    RARITY_NAMES: ["凡品", "良品", "上品", "極品", "神品"],
+    REALM_NAMES: ["凡人", "練氣初期", "練氣中期", "練氣後期", "築基初期", "築基中期", "築基後期", "結丹期", "元嬰期"]
+};
